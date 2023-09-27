724eeeac05eb8c9ca7e8f48c37205daa85de787c
ar-bebop <aribeir4@gmail.com>
2023-09-27 00:39:07 -0400

    config

--- config.h
+++ config.h
@@ -54,7 +54,7 @@ int allowwindowops = 0;
  * low minlatency will tear/flicker more, as it can "detect" idle too early.
  */
 static double minlatency = 8;
-static double maxlatency = 33;
+static double maxlatency = 16;
 
 /*
  * blinking timeout (set to 0 to disable blinking) for the terminal blinking
